0dc636f2128df06f867921e6f168bde373f13a01
TryZorce <christopher.moron@gmail.com>
2023-12-04 11:23:33 +0100

    Invoice scss

--- next/src/app/invoice/page.tsx
+++ next/src/app/invoice/page.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import './invoice.scss';
+import Header from '@/ui/organisms/Header';
+import Footer from '@/ui/organisms/Footer';
 
 const Invoice: React.FC = () => {
   return (
+    <>
+    <Header />
     <div className="invoice-container">
-      <h1>Confirmation de Commande</h1>
-      <p>Merci pour votre commande !</p>
+    <h1>Confirmation de Commande</h1>
+    <p>Merci pour votre commande !</p>
+    <img src="/img/confirmation_commande.png" alt="Confirmation de commande"/>
     </div>
+    <Footer />
+    </>
   );
 };
 
