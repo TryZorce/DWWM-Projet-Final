114d5cb2bd60552cdcbf247a3cb0d9bd54f1cff8
TryZorce <christopher.moron@gmail.com>
2023-12-05 11:03:48 +0100

    Ajout contenue statique Home

--- next/src/app/page.tsx
+++ next/src/app/page.tsx
@@ -6,18 +6,35 @@ import Footer from './ui/organisms/Footer';
 import CategoryList from './ui/molecules/CategoryList';
 import SearchBar from './ui/organisms/Searchbar';
 import HeroBanner from './ui/molecules/HeroBanner';
-
+import "./style.scss"
 const Page = () => {
 
-  
+
   return (
     <div>
-      <Header/>
-      <SearchBar/>
-      <HeroBanner/>
-      <CategoryList/>
-      <ArticleList/>
-      <Footer/>
+      <Header />
+      <SearchBar />
+      <HeroBanner />
+      <div className='container-home'>
+      <h1 className="title">Bienvenue dans l'univers envoûtant des bougies</h1>
+
+      <p className="intro">Explorez des créations artisanales conçues pour éveiller vos sens et transformer votre espace en un sanctuaire de tranquillité.</p>
+
+      <p>Chaque bougie, une histoire. Chaque lueur, une invitation à la détente.</p>
+
+      <ul className="features">
+        <li>Découvrez des fragrances envoûtantes</li>
+        <li>Transformez votre espace en un sanctuaire de tranquillité</li>
+        <li>Créez des moments magiques avec nos bougies</li>
+      </ul>
+
+      <p className="description">Explorez notre collection pour trouver la parfaite harmonie entre fragrances envoûtantes et design raffiné.</p>
+
+      <p>Chez nous, chaque flamme est un récit, chaque lumière, une émotion.</p>
+      </div>
+      <CategoryList />
+      <ArticleList />
+      <Footer />
     </div>
   );
 };
